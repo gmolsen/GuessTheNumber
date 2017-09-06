f0084e27128b2bac8a06975122a88dc524bb117b
Greg <gmolsenvideo@gmail.com>
2017-09-06 12:16:46 -0400

    Foundation of "Guess A Number" game complete, not able to run game multiple times.

--- GuessTheNumberProject/Program.cs
+++ GuessTheNumberProject/Program.cs
@@ -5,8 +5,60 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GuessTheNumberProject {
-	class Program  {
+	class Program {
+		int GenerateMagicNumber(int HighestNumber) {
+			Random rnd = new Random();
+			var MagicNumber = rnd.Next(101);
+			// Debug($"The magic number is {MagicNumber}");
+			return MagicNumber;
+		}
+		int AskForTheGuess() {
+			Console.Write($"Enter your guess please : ");
+			var TheGuess = Console.ReadLine();
+			int GuessTheNumber = int.Parse(TheGuess);
+			return GuessTheNumber;
+		}
+		int CompareGuessToMagicNumber(int MagicNumber, int TheGuess) {
+			if (MagicNumber == TheGuess) { // is the guess correct?
+				return 0;
+			}
+			if (MagicNumber > TheGuess) { // is the guess too low?
+				return -1;
+			}
+			if (MagicNumber < TheGuess) { // is the guess too high?
+				return 1;
+			}
+
+			return -2;
+		}
+		bool PrintOutcomeResult(int Result) {
+			if (Result == 0) { // the guess matched - they won
+				Debug("Correct! You won the game!");
+				return true; }
+
+			if (Result == -1) { // the guess is too low
+				Debug("Too low, guess again.");
+				return false; }
+
+			if (Result == 1) { // the guess is too high
+			Debug("Too high, guess again.");
+			return false; }
+			return true;
+		}
+		void Debug(string message) {
+			Console.WriteLine(message);
+		}
+		void Run() {
+			var MagicNumber = GenerateMagicNumber(100);
+			bool GameOver = false;
+			while (GameOver == false) {
+			var UserGuess = AskForTheGuess();
+			var GuessResult = CompareGuessToMagicNumber(MagicNumber, UserGuess);
+			GameOver = PrintOutcomeResult(GuessResult);
+			}
+		}
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
